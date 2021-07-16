b270cbd13c29398901d9a320bd35fe5388b725af
KimGreenbush <kimgreenbush@gmail.com>
2021-07-15 17:41:00 -0800

    replace default comps, add Profile

---
+++ components/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <div>This will be an image of me.</div>
+    <h1>Kimberley Greenbush</h1>
+    <div>These will be social links.</div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
